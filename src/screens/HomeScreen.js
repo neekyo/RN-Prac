1060e5e36c862da568d90c895c87fb35e90109d7
Nick Sherman <ogleffeckt2@yahoo.com>
2019-11-11 16:11:18 -0500

    added 3 screens

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+
+const HomeScreen = () => {
+	return (
+		<View>
+			<Text style={styles.text}>Hi there!</Text>
+			<Button onPress={() => console.log('Button pressed')} title="Go to Components Demo" />
+      <TouchableOpacity>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 30
+	}
+});
+
+export default HomeScreen;
