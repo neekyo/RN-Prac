78fe12f564cef129fd55024764f088c133e2c5f4
Nick Sherman <ogleffeckt2@yahoo.com>
2019-11-14 12:49:32 -0500

    added image props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ const HomeScreen = ({ navigation }) => {
 			<Text style={styles.text}>Hi there!</Text>
 			<Button onPress={() => navigation.navigate('Components')} title="Go to Components Demo" />
 			<Button title="Go to List Demo" onPress={() => navigation.navigate('List')} />
-			<Button title="Go to Image Screen" onPress={() => navigation.navigate('Image')} />
+			<Button title="Go to Image Demo" onPress={() => navigation.navigate('Image')} />
 		</View>
 	);
 };
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
 	text: {
 		fontSize: 30
 	}
-});
+});	
 
 export default HomeScreen;
