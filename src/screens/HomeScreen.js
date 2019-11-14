4d61c0a66f51364733c232c8384b3b8d3cc7bf2e
Nick Sherman <ogleffeckt2@yahoo.com>
2019-11-14 14:21:02 -0500

    added image scores & counter screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,6 +8,7 @@ const HomeScreen = ({ navigation }) => {
 			<Button onPress={() => navigation.navigate('Components')} title="Go to Components Demo" />
 			<Button title="Go to List Demo" onPress={() => navigation.navigate('List')} />
 			<Button title="Go to Image Demo" onPress={() => navigation.navigate('Image')} />
+			<Button title="Go to Counter Demo" onPress={() => navigation.navigate('Counter')} />
 		</View>
 	);
 };
@@ -16,6 +17,6 @@ const styles = StyleSheet.create({
 	text: {
 		fontSize: 30
 	}
-});	
+});
 
 export default HomeScreen;
