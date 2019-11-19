80e5295806469016ea5b2f628fe1e0661ae71f5b
Nick Sherman <ogleffeckt2@yahoo.com>
2019-11-19 17:13:27 -0500

    misc styling

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,23 +3,37 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
 	return (
-		<View style={styles.viewStyle}>
-			<Text style={styles.textStyle}>Child #1</Text>
-			<Text style={styles.textStyle}>Child #2</Text>
-			<Text style={styles.textStyle}>Child #3</Text>
+		<View style={styles.parentStyle}>
+			<View style={styles.view0neStyle} />
+			<View style={styles.viewTwoStyle} />
+			<View style={styles.viewThreeStyle} />
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	viewStyle: {
+	parentStyle: {
 		borderWidth: 3,
 		borderColor: 'black',
-		alignItems: 'flex-start'
+		height: 200,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
 	},
-	textStyle: {
-		borderWidth: 3,
-		borderColor: 'red'
+	view0neStyle: {
+		height: 50,
+		width: 50,
+		backgroundColor: 'red'
+	},
+	viewTwoStyle: {
+		height: 50,
+		width: 50,
+		backgroundColor: 'green',
+		top: 50
+	},
+	viewThreeStyle: {
+		height: 50,
+		width: 50,
+		backgroundColor: 'purple'
 	}
 });
 
